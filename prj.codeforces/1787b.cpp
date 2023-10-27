89833ecd1e7d9d62461e2e7377ab061e32bfe39e
irina babynina <115956515+irisich@users.noreply.github.com>
2023-10-27 09:19:06 +0300

    Create 1787b.cpp

---
+++ prj.codeforces/1787b.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        ll x=n;
+        ll i=2;
+        map<ll,ll> m;
+        ll mx=0;
+        while(!(x==1) && i<=sqrt(n)){
+            if(x%i==0){
+                while(x%i==0){
+                    x=x/i;
+                    m[i]++;
+                    mx=max(mx,m[i]);
+                }
+            }
+            i++;
+        }
+        ll sum=x;
+        for(auto it:m){
+            sum=sum*(it.first);
+            m[it.first]--;
+        }
+        ll p=1;
+        for(ll i=0; i<mx-1; i++){
+            p=1;
+            for(auto it:m){
+                if(m[it.first]!=0){
+                    p=p*(it.first);
+                    m[it.first]--;
+                }
+            }
+            sum+=p;
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}
