346308907394f22f3bca41664c97d43ca355587d
irina babynina <115956515+irisich@users.noreply.github.com>
2023-12-11 13:37:51 +0300

    Add files via upload

---
+++ prj.cw/ContentChecker.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+// Функция для чтения текста с изображения с использованием Tesseract
+string readTextFromImage(Mat& image) {
+    tesseract::TessBaseAPI tess;
+    tess.Init(NULL, "eng", tesseract::OEM_DEFAULT);
+    tess.SetPageSegMode(tesseract::PSM_AUTO);
+
+    tess.SetImage(image.data, image.cols, image.rows, 3, image.step);
+    char* outText = tess.GetUTF8Text();
+    string result(outText);
+    delete[] outText;
+
+    return result;
+}
+
+// Функция для проверки содержания в начале файла
+bool checkContentAtBeginning(const vector<Mat>& images, int startPage, int endPage) {
+    for (int i = startPage; i <= endPage; ++i) {
+        string text = readTextFromImage(images[i]);
+
+        // Добавьте код для проверки содержания в начале файла
+        // Например, можно проверить, есть ли определенные ключевые слова или фразы в тексте
+
+        // Пример:
+        if (text.find("Начало файла") == string::npos) {
+            cout << "Ошибка: Не обнаружено содержание в начале файла на странице " << i + 1 << endl;
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Функция для проверки оформления
+bool checkFormatting(const vector<Mat>& images, int startPage, int endPage) {
+    // Добавьте код для проверки оформления
+    // Проверьте порядок блоков, нумерацию и другие критерии
+
+    // Пример:
+    for (int i = startPage; i <= endPage; ++i) {
+        string text = readTextFromImage(images[i]);
+
+        // Проверка порядка блоков
+        if (text.find("Блок 1") == string::npos) {
+            cout << "Ошибка: Не найден блок 1 на странице " << i + 1 << endl;
+            return false;
+        }
+
+        // Добавьте другие проверки по необходимости
+    }
+
+    return true;
+}
+
+// Функция для проверки оглавления
+bool checkTableOfContents(const vector<Mat>& images, int startPage, int endPage) {
+    // Добавьте код для проверки оглавления
+    // Проверьте, что все блоки перечислены в правильном порядке и никакие не пропущены
+
+    // Пример:
+    string expectedBlocks[] = { "Блок 1", "Блок 2", "Блок 3" };
+
+    for (int i = startPage; i <= endPage; ++i) {
+        string text = readTextFromImage(images[i]);
+
+        for (const string& block : expectedBlocks) {
+            if (text.find(block) == string::npos) {
+                cout << "Ошибка: Не найден блок '" << block << "' в оглавлении на странице " << i + 1 << endl;
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    // Загрузка изображений в массив images (cv::Mat)
+    // ...
+
+    int startPage = 0; // Номер первой страницы
+    int endPage = images.size() - 1; // Номер последней страницы
+
+    // Проверка содержания в начале файла
+    if (!checkContentAtBeginning(images, startPage, endPage)) {
+        return 1;
+    }
+
+    // Проверка оформления
+    if (!checkFormatting(images, startPage, endPage)) {
+        return 2;
+    }
+
+    // Проверка оглавления
+    if (!checkTableOfContents(images, startPage, endPage)) {
+        return 3;
+    }
+
+    cout << "Все проверки пройдены успешно." << endl;
+
+    return 0;
+}
