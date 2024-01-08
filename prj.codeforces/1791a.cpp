ed24cf8c968782545a1b4bfc17e403699d865fde
irina babynina <115956515+irisich@users.noreply.github.com>
2024-01-08 19:19:06 +0300

    Create 1791a.cpp

---
+++ prj.codeforces/1791a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+    int t;
+    cin>>t;
+    while (t--){
+        char c;
+        cin>>c;
+        if (c=='c' || c == 'o' || c =='d' || c=='e' ||c=='f' || c == 'r' || c=='s'){
+            cout << "yes"<<endl;
+        }
+        else{
+        cout<<"no"<<endl;
+        }
+    }
+    return 0; 
+}
