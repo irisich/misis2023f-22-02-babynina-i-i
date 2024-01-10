996740021b1d112fe8023a9c16f05bff2ed92a49
irina babynina <115956515+irisich@users.noreply.github.com>
2024-01-11 01:01:38 +0300

    Delete ContentChecker — копия directory

--- ContentChecker — копия/html/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['fileparser_0',['FileParser',['../class_file_parser.html',1,'FileParser'],['../class_file_parser.html#acb58172b4650cd8015d5e8cad7d29647',1,'FileParser::FileParser()']]]
-];
--- ContentChecker — копия/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['contentchecker_2ecpp_0',['contentChecker.cpp',['../content_checker_8cpp.html',1,'']]]
-];
